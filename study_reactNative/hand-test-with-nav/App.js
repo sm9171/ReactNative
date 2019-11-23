58e29bea9c8de541bc33c98a19191d09881bc61d
na <sm9171@nate.com>
2019-11-23 15:26:31 +0900

    navigation

---
+++ study_reactNative/hand-test-with-nav/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './pages/Home';
+import TypeA from './pages/TypeA';
+import TypeB from './pages/TypeB';
+import TypeC from './pages/TypeC';
+import TypeD from './pages/TypeD';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+// 여러개의 네비게이터를 생성할 수 있고 서로 겹치는 상태로 생성할 수도 있음
+const Navigator = createStackNavigator( {
+  Home: Home,
+  TypeA: TypeA,
+  TypeB: TypeB,
+  TypeC: TypeC,
+  TypeD: TypeD,
+} );
+// 네비게이터들을 하나로 묶어서 표시하는 컨테이너
+const Container = createAppContainer( Navigator );
+
+export default function App() {
+  return (
+    <Container/>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
