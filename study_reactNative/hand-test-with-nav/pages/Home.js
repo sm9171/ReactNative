5274165129ce77f0f37e7714c8640efbc93368fd
na <sm9171@nate.com>
2019-11-23 15:44:08 +0900

    navigation

--- study_reactNative/hand-test-with-nav/pages/Home.js
+++ study_reactNative/hand-test-with-nav/pages/Home.js
@@ -6,28 +6,34 @@ import TypeA from '../assets/hand-type-a.png';
 import TypeB from '../assets/hand-type-b.png';
 import TypeC from '../assets/hand-type-c.png';
 import TypeD from '../assets/hand-type-d.png';
+import Center from '../components/Center';
+
 
 const imageStyle = { width: 136, height: 175 };
 
 const Home = props => {
     return (
         <SafeAreaView>
-            <HGroup>
-                <TouchableOpacity onPress={ () => props.navigation.navigate( 'TypeA' )}>
-                    <Image source={ TypeA } style={ imageStyle }/>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={ () => props.navigation.navigate( 'TypeB' )}>
-                    <Image source={ TypeB } style={ imageStyle }/>
-                </TouchableOpacity>
-            </HGroup>
-            <HGroup>
-                <TouchableOpacity onPress={ () => props.navigation.navigate( 'TypeC' )}>
-                    <Image source={ TypeC } style={ imageStyle }/>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={ () => props.navigation.navigate( 'TypeD' )}>
-                    <Image source={ TypeD } style={ imageStyle }/>
-                </TouchableOpacity>
-            </HGroup>
+            <Center>
+                <HGroup>
+                    <TouchableOpacity onPress={ () => props.navigation.navigate( 'TypeA' )}>
+                        <Image source={ TypeA } style={ imageStyle }/>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={ () => props.navigation.navigate( 'TypeB' )}>
+                        <Image source={ TypeB } style={ imageStyle }/>
+                    </TouchableOpacity>
+                </HGroup>
+            </Center>
+            <Center>
+                <HGroup>
+                    <TouchableOpacity onPress={ () => props.navigation.navigate( 'TypeC' )}>
+                        <Image source={ TypeC } style={ imageStyle }/>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={ () => props.navigation.navigate( 'TypeD' )}>
+                        <Image source={ TypeD } style={ imageStyle }/>
+                    </TouchableOpacity>
+                </HGroup>
+            </Center>
         </SafeAreaView>
     );
 }
